84b9212be4954f336760263644d3b40f4c1a2b17
rainu <rainu@raysha.de>
2019-09-09 17:39:38 +0200

    adding skelleton for different note types

---
+++ components/note/Text.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" md="4">
+        <v-text-field label="Legal first name*" required></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="6" md="4">
+        <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="6" md="4">
+        <v-text-field
+          label="Legal last name*"
+          hint="example of persistent helper text"
+          persistent-hint
+          required
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field label="Email*" required></v-text-field>
+      </v-col>
+      <v-col cols="12">
+        <v-text-field label="Password*" type="password" required></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-select
+          :items="['0-17', '18-29', '30-54', '54+']"
+          label="Age*"
+          required
+        ></v-select>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-autocomplete
+          :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+          label="Interests"
+          multiple
+        ></v-autocomplete>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "NoteText",
+    data: () => ({
+      dialog: false,
+    }),
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,80 @@
     justify-center
     align-center
   >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      Hello World
+    <v-flex xs12 sm8 md6>
+
+      <v-dialog v-model="note.dialog.open" persistent>
+        <v-card v-if="note.dialog.type">
+          <v-card-title>
+            <span class="headline">{{$t(note.dialog.type.text)}}</span>
+          </v-card-title>
+          <v-card-text>
+            <NoteText v-if="note.dialog.type.id === 'text'"></NoteText>
+            <NotePicture v-if="note.dialog.type.id === 'picture'"></NotePicture>
+          </v-card-text>
+          <v-card-actions>
+            <div class="flex-grow-1"></div>
+            <v-btn color="error" @click="note.dialog.open = false">Close</v-btn>
+            <v-btn color="primary" @click="note.dialog.open = false">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-footer app class="pa-0">
+        <v-toolbar dense>
+          <div class="flex-grow-1"></div>
+
+          <v-toolbar-items>
+            <v-menu offset-y top>
+              <template v-slot:activator="{ on }">
+                <v-btn v-on="on">
+                  <v-icon>add_circle</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item v-for="type in note.types" :key="type.id">
+                  <v-btn block @click="onNewNote(type)">
+                    <v-icon>{{type.icon}}</v-icon>
+                    {{$t(type.text)}}
+                  </v-btn>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-toolbar-items>
+        </v-toolbar>
+      </v-footer>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import NoteText from "../components/note/Text";
+import NotePicture from "../components/note/Text";
+
 export default {
-  components: {}
+  components: {
+    NoteText, NotePicture
+  },
+  data(){
+    return {
+      note: {
+        dialog: {
+          open: false,
+          type: null
+        },
+        data: {},
+        types: [
+          { id: 'picture', icon: 'photo', text: 'note.type.picture' },
+          { id: 'text', icon: 'notes', text: 'note.type.text' },
+        ]
+      }
+    }
+  },
+  methods: {
+    onNewNote(type) {
+      this.note.dialog.open = true
+      this.note.dialog.type = type
+    }
+  }
 }
 </script>
