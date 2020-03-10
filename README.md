d8d806aef2c833946ba95366e51906ddc8ce2254
rainu <rainu@raysha.de>
2020-03-10 21:06:15 +0100

    autofocus the password field at unlock screen

--- README.md
+++ README.md
@@ -64,6 +64,9 @@ For detailed explanation on how things work, check out [Nuxt.js docs](https://nu
 
 ## Changelog
 
+### 0.9.15
+* autofocus the password field at unlock screen
+
 ### 0.9.14
 * dont force usage of indexedDB so that DevNotes als work with older browsers
 
--- pages/unlock/index.vue
+++ pages/unlock/index.vue
@@ -1,36 +1,43 @@
 <template>
-  <div>
-    <v-dialog :value="true" persistent>
-      <v-card>
-        <v-card-title>
-          <span class="headline">{{$t('encryption.unlock')}}</span>
-        </v-card-title>
+  <v-container fill-height>
 
-        <v-card-text>
-          <v-form @submit.prevent="onUnlock">
-            <v-text-field :type="showPassword ? 'text' : 'password'"
-                          :label="$t('encryption.password')"
-                          v-model="password"
-                          :append-icon="showPassword ? 'visibility' : 'visibility_off'"
-                          @click:append="showPassword = !showPassword"/>
-          </v-form>
-        </v-card-text>
+    <v-row align="center" justify="center">
+      <v-col>
+        <v-card>
+          <v-card-title>
+            <span class="headline">{{$t('encryption.unlock')}}</span>
+          </v-card-title>
 
-        <v-card-actions>
-          <div class="flex-grow-1"></div>
-          <v-btn color="primary" type="submit" form="login-form" :disabled="!password" @click="onUnlock">{{$t('encryption.actions.unlock')}}</v-btn>
-        </v-card-actions>
+          <v-card-text>
+            <v-form @submit.prevent="onUnlock">
+              <v-text-field :type="showPassword ? 'text' : 'password'"
+                            autofocus
+                            :label="$t('encryption.password')"
+                            v-model="password"
+                            :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+                            @click:append="showPassword = !showPassword"/>
+            </v-form>
+          </v-card-text>
 
-      </v-card>
-    </v-dialog>
+          <v-card-actions>
+            <div class="flex-grow-1"></div>
+            <v-btn color="primary" type="submit" form="login-form" :disabled="!password" @click="onUnlock">{{$t('encryption.actions.unlock')}}</v-btn>
+          </v-card-actions>
 
-    <v-snackbar v-model="snackbar" color="error" class="text-center" :timeout="5000">
-      {{$t('encryption.invalid')}}
-      <v-btn text @click="snackbar = false" >
-        <v-icon>close</v-icon>
-      </v-btn>
-    </v-snackbar>
-  </div>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-snackbar v-model="snackbar" color="error" class="text-center" :timeout="5000">
+          {{$t('encryption.invalid')}}
+          <v-btn text @click="snackbar = false" >
+            <v-icon>close</v-icon>
+          </v-btn>
+        </v-snackbar>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
